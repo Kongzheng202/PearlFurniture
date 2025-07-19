71fb500d3c8d90e9c4e0d5c1fd9c8e5cb49c53ca
Kongzheng202 <yusukekong@gmail.com>
2025-07-20 04:33:39 +0800

    website

---
+++ PearlFurniture/Areas/Identity/Data/PearlFurnitureContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using PearlFurniture.Areas.Identity.Data;
+
+namespace PearlFurniture.Data;
+
+public class PearlFurnitureContext : IdentityDbContext<PearlFurnitureUser>
+{
+    public PearlFurnitureContext(DbContextOptions<PearlFurnitureContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        // Customize the ASP.NET Identity model and override the defaults if needed.
+        // For example, you can rename the ASP.NET Identity table names and more.
+        // Add your customizations after calling base.OnModelCreating(builder);
+    }
+}
--- PearlFurniture/Program.cs
+++ PearlFurniture/Program.cs
@@ -1,7 +1,18 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using PearlFurniture.Data;
+using PearlFurniture.Areas.Identity.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("PearlFurnitureContextConnection") ?? throw new InvalidOperationException("Connection string 'PearlFurnitureContextConnection' not found.");
+
+builder.Services.AddDbContext<PearlFurnitureContext>(options => options.UseSqlServer(connectionString));
+
+builder.Services.AddDefaultIdentity<PearlFurnitureUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<PearlFurnitureContext>();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
 
 var app = builder.Build();
 
@@ -9,19 +20,27 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
+
+
+app.UseDefaultFiles(); // This looks for index.html, default.html, etc. in wwwroot
+
+
 app.UseStaticFiles();
 
 app.UseRouting();
-
+app.UseAuthentication(); // after app.UseRouting()
 app.UseAuthorization();
 
+
+// Optional: Keep MVC routing if you're using controllers
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
+app.MapRazorPages(); // after app.MapControllerRoute
 app.Run();
+
