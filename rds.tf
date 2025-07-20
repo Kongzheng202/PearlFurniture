1238e761e9d5553ca2deebc3e6d41832c1aa9377
Kongzheng202 <yusukekong@gmail.com>
2025-07-21 06:07:08 +0800

    rds

--- rds.tf
+++ rds.tf
@@ -41,17 +41,17 @@ resource "aws_security_group" "sql_sg" {
 }
 
 resource "aws_db_instance" "mssql" {
-  allocated_storage      = 20
-  engine                 = "sqlserver-ex"
-  engine_version         = "15.00.4043.16.v1"
-  instance_class         = "db.t3.micro"
-  username               = var.db_username
-  password               = var.db_password
+  allocated_storage = 20
+  engine            = "sqlserver-ex"
+  engine_version    = "15.00.4043.16.v1"
+  instance_class    = "db.t3.micro"
+  username          = var.db_username
+  password          = var.db_password
 
-  publicly_accessible    = true  # <--- allow connection from your local machine
+  publicly_accessible = true # <--- allow connection from your local machine
 
-  multi_az               = false
-  db_subnet_group_name   = aws_db_subnet_group.sql_subnet_group.name
+  multi_az             = false
+  db_subnet_group_name = aws_db_subnet_group.sql_subnet_group.name
 
   # Use sql_sg (which includes your IP)
   vpc_security_group_ids = [aws_security_group.sql_sg.id]
