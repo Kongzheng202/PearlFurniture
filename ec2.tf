402da059c5d86087eedf7a26778b65ecbb985298
Kongzheng202 <yusukekong@gmail.com>
2025-07-21 17:15:48 +0800

    ec2

--- ec2.tf
+++ ec2.tf
@@ -63,7 +63,7 @@ resource "aws_security_group" "backend_sg" {
     from_port       = 1433
     to_port         = 1433
     protocol        = "tcp"
-    security_groups = [aws_security_group.web_sg.id] 
+    security_groups = [aws_security_group.web_sg.id]
   }
 
   egress {
